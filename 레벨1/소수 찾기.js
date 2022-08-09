e2c53d4044082af2d8cf4c83f2a24d2a5f83edeb
seungho <fbtmdgh4517@naver.com>
2022-08-09 22:46:41 +0900

    first commit

---
+++ 레벨1/소수 찾기.js
@@ -0,0 +1,20 @@
+function solution(n) {
+    let answer = 0;
+
+    for (let i = 2; i <= n; i++) {
+        let count = 0;
+
+        for (let j = 2; j <= Math.sqrt(i); j++) {
+            if (i % j === 0) {
+                count++;
+                break;
+            }
+        }
+
+        if (count === 0) {
+            answer++;
+        }
+    }
+
+    return answer;
+}
